1aa8db0c08e8b32c437b70736248e88b4430a296
Qwroth <white_zetsu@hotmail.com>
2018-07-26 22:59:36 +0800

    Added more CSS and bootstrap

--- index.php
+++ index.php
@@ -4,23 +4,34 @@
 <script type="text/javascript" language="javascript" src="javascripts/jquery.js"></script>
 <script type="text/javascript" language="javascript" src="javascripts/myScript.js"></script>
 <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
+<link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" />
+
+<img id ="banner" src="banner.gif" alt="Banner Image"/>
+
 </head>
 
+<style>  
 
+body 
+{
+	background-color: lightblue; 
+}
+
+</style>
 
 <body>
 
-<h1 class="heading"> Please enter details below </h1>
+<h1 class="heading" align="center"> Please enter details below </h1>
 
-<table >
+<table align ="center">
 
 <form id="form" action ="register.php"> 
-<tr><td>Chose Username: </td><td><input type="text" id="form_username" class="form_text"></td><td><span  id="username_error_message" class="error_message"></span></td></tr>
-<tr><td>Email Address: </td><td><input type="text" id="form_email"class="form_text"></td><td><span  id="email_error_message"class ="error_message"></span></td></tr>
-<tr><td>Date of Birth: </td><td><input type="text" id="form_dob" placeholder= "DD/MM/YYYY"class="form_text"></td><td><span  id="dob_error_message"class="error_message"></span></td></tr>
-<tr><td>Password: </td><td><input type="password" id="form_password"class="form_text"></td><td><span  id="password_error_message" class="error_message"></span></td></tr>
-<tr><td>Retype Password: </td><td><input type="password" id="form_retype_password" class="form_text"></td><td><span  id="retype_password_error_message" class="error_message"></span></td></tr>
-<tr><td> <input type="submit" id="submit_button" value="Submit"class="submit_button"> <input type="button" id="clear_button" value="Clear"class="clear_button"></td><td> </td> </tr>
+<tr><td>Chose Username: </td><td><input type="text" id="form_username" class="form-control"></td><td><span  id="username_error_message" class="error_message"></span></td></tr>
+<tr><td>Email Address: </td><td><input type="text" id="form_email"class="form-control" placeholder="example@hotmail.com"></td><td><span  id="email_error_message"class ="error_message"></span></td></tr>
+<tr><td>Date of Birth: </td><td><input type="text" id="form_dob" placeholder= "DD/MM/YYYY"class="form-control"></td><td><span  id="dob_error_message"class="error_message"></span></td></tr>
+<tr><td>Password: </td><td><input type="password" id="form_password"class="form-control"></td><td><span  id="password_error_message" class="error_message"></span></td></tr>
+<tr><td>Retype Password: </td><td><input type="password" id="form_retype_password" class="form-control"></td><td><span  id="retype_password_error_message" class="error_message"></span></td></tr>
+<tr><td> <input type="submit" id="submit_button" value="Submit"class="btn"> <input type="button" id="clear_button" value="Clear"class="btn"></td><td> </td> </tr>
 
 
 
--- register.php
+++ register.php
@@ -14,9 +14,66 @@
 
 
 
-<h1>The form has been submitted!</h1>
+
+<h1>The form has been submitted, no errors!</h1>
 
 
 
 </body>
 </html>
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";  
+$db = "test"; 
+$conn = new mysqli($servername, $username, $password, $db); 
+
+
+
+
+$sql = "SELECT * FROM users";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0)
+{
+	echo "<table>"; 
+	echo "<tr>";
+	echo "<th>ID</th>"; 
+	echo "<th> Username  </th>"; 
+	echo "<th> Email </th>"; 
+	echo "<th> Date of birth </th>";
+	echo "<th> Password</th>";
+	echo "</tr>"; 
+
+	while($row = $result->fetch_assoc())
+	{
+		
+		echo "<tr>";
+		echo "<td>";
+		echo $row["ID"];
+		echo "</td>";
+		echo "<td>";
+		echo $row["username"];
+		echo "</td>"; 
+		echo "<td>";
+		echo $row["email"];
+		echo "</td>"; 
+		echo "<td>";
+		echo $row["dob"];
+		echo "</td>"; 
+		echo "<td>";
+		echo $row["password"];
+		echo "</td>"; 
+		echo "</tr>"; 
+
+	}
+
+	echo "</table>"; 
+}
+
+
+
+?>
